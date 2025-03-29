464d96d15e0ffd2aa06dba46322445be55fb5d17
Ali Abbas <alizaidi22003@gmail.com>
2025-03-29 16:54:45 +0500

    changed the entire strucute and added the code for login.

--- learning/backend/app.js
+++ learning/backend/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/ali', (req, res) => {
+  res.send('Hello World! ali')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
