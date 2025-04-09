1677dec97a42acb73fac521dbb68e0cc1a657266
Mohammad Mustafa <163987556+mohmmddd00@users.noreply.github.com>
2025-04-09 20:13:26 +0500

    booking schema created

---
+++ server/models/booking.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const BookingSchema = new mongoose.Schema({
+    track: {
+        type: String,
+        required: [true, "Please provide the track"],
+        enum: ["2f2f", "SportsZilla", "track 3", "track 4"],
+    },
+
+    timeSlot: {
+        type: String, 
+        required: [true, "Please provide a time slot"],
+        match: [
+            /^([01]\d|2[0-3]):([0-5]\d)$/, // Matches HH:mm format (24-hour time)
+            "Please provide a valid timeslot!",
+          ],
+    },
+
+    date: {
+        type: Date,
+        required: [true, "Please provide a date"],
+    },
+
+    email: {
+        type: String,
+        required: [true, "Please provide a valid email"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[a-zA-Z\-0-9]+\.[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+          ],
+    },
+
+    bookingCreationDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Booking", BookingSchema);
