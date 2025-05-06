21ac2e3caf6953afd0d046159bf02758e6b1772c
Ali Abbas <alizaidi22003@gmail.com>
2025-05-06 17:40:03 +0500

    lol

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
+
+// https://vitejs.dev/config/
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './' // ← This is important for relative asset paths
 })
